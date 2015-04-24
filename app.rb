c3bb070bb3326ccdaaf5d913aed4f0c538efb08e
David Abel <drabelpdx@gmail.com>
2015-04-24 14:14:56 -0700

    Adds intergration spec to return 3 happys. Fails

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word_count')
+also_reload('lib/**/*.rb')
+
+get('/') do
+    erb(:index)
+end
+
+get('/special') do
+    @special = params.fetch('special').wordcount()
+    erb(:special)
+end
