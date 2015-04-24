dcd0a6c61bb90bc70589bd9e85dfcf075994a517
David Abel <drabelpdx@gmail.com>
2015-04-24 13:18:38 -0700

    Passes second spec of multiple words against a fixed word

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,14 +1,22 @@
 class String
     define_method(:wordcount) do
-      input = self
-      counter = 1
-      counts = 0
+
+     wordstring = self.split(/\W+/)
+     counter = 0
+     wordstring.each do
+         counter += 1
+     end
+     counts = 0
+     index = 0
+#binding.pry
      until counter < 1
          counter -= 1
-        if self == "happy"
+        if wordstring.fetch(index).to_str == "happy"
           counts += 1
         end
+        index +=1
       end
-     return counts
+#binding.pry
+      return counts
   end
-end
+ end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,11 +3,16 @@ require('word_count')
 require('pry')
 
 describe('String#wordcount') do
-   it("should count the number of times the input word appears") do
-   expect("happy".wordcount()).to(eq(1))
-   end
+#   it("should count the number of times the input word appears") do
+ #  expect("happy".wordcount()).to(eq(1))
+  # end
 
     it("should count the number of times the input word appears in a string i.e. sentence") do
     expect("I am so very happy, happy, happy to be getting my code reviewed today.".wordcount()).to(eq(3))
     end
+#    it("should count the number of times the input word appears in a string i.e. sentence") do
+#    expect("I am happy happy happy".wordcount()).to(eq(3))
+#    end
+
+
 end
