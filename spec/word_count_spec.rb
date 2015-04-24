0ce5583e18b087387ea3d1cbb83adcc5a35ddc88
David Abel <drabelpdx@gmail.com>
2015-04-24 09:46:41 -0700

    Adds first spec to count the number of times the input word appears and fails first test

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,8 +1,8 @@
 require('rspec')
 require('word_count')
 
-describe("String#wordcount") do
-   it("") do
-   expect("".method()).to(eq())
+describe('String#wordcount') do
+   it("should count the number of times the input word appears") do
+   expect("happy".wordcount()).to(eq(1))
    end
 end
