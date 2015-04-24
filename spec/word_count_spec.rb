cd49b71c365d8246497a5f5fd4334c48d66006e0
David Abel <drabelpdx@gmail.com>
2015-04-24 11:15:51 -0700

    Adds second spec to count input word in a sentence

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,4 +6,8 @@ describe('String#wordcount') do
    it("should count the number of times the input word appears") do
    expect("happy".wordcount()).to(eq(1))
    end
+
+    it("should count the number of times the input word appears in a string i.e. sentence") do
+    expect("I am so very happy, happy, happy to be getting my code reviewed today.".wordcount()).to(eq(3))
+    end
 end
